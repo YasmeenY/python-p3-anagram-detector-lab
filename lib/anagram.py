20c0d5ca5121806684e728adf3d1e321ca99be9a
Yasmeen Yousef <yasmeen.yousef05@gmail.com>
2023-05-02 13:59:29 -0400

    lab complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.letters = sorted([letter for letter in word])
+    def match(self, words):
+        self.words = words
+        matching = list()
+        for wording in words:
+            if sorted([letter for letter in wording]) == self.letters:
+                matching.append(wording)
+        return matching
